131761fdcca5e3e718c3ff6587a86019af601f60
Michele M <mnmin@gmail.com>
2022-07-06 15:59:51 +0100

    Render three types of breweries only

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const breweriesList = document.querySelector("#breweries-list");
 const apiURL = "https://api.openbrewerydb.org/breweries";
 
 const state = {
-  types: ["Mirco", "Reional", "Brewpub"],
+  types: ["micro", "regional", "brewpub"],
 
   usState: "",
 
@@ -18,29 +18,35 @@ function searchByStateListener() {
     state.usState = event.target[0].value;
     fetch(`${apiURL}?by_state=${state.usState}&per_page=100`)
       .then((res) => {
-        //console.log("my fetch", res);
         return res.json();
       })
-      .then((breweries) => {
-        //console.log("my data", data);
-        state.breweries = breweries;
+      .then((data) => {
+        setBreweriesData(data);
         render();
       });
   });
 }
 
+function setBreweriesData(breweries) {
+  const filterBreweries = breweries.filter((brewery) =>
+    state.types.includes(brewery.brewery_type)
+  );
+  state.breweries = filterBreweries;
+}
+
+
 function filterByBreweryTypeListener() {
-    const selectBreweryTypeButton = document.querySelector('#filter-by-type')
+  const selectBreweryTypeButton = document.querySelector("#filter-by-type");
 
-    selectBreweryTypeButton.addEventListener("submit", (event) => {
-        state.types = event.target.value;
+  selectBreweryTypeButton.addEventListener("submit", (event) => {
+    state.types = event.target.value;
 
-        if(brew_type.length > 0) {
-            selectBreweryTypeButton = state.usState.filter(brewery => {
-                return brewery.brew_type === state.types
-            })
-        }
-    })
+    if (brew_type.length > 0) {
+      selectBreweryTypeButton = state.usState.filter((brewery) => {
+        return brewery.brew_type === state.types;
+      });
+    }
+  });
 }
 
 function initalise() {
@@ -48,6 +54,10 @@ function initalise() {
 }
 
 function renderBrewery(brewery) {
+    // const liE1 = document.createElement('li');
+    // const h2E1 = document.createElement('h2');
+    // const divE1 = document.createElement('div');
+
   const li = document.createElement("li");
   li.innerHTML = `<h2>${brewery.name}</h2>
   <div class="type">${brewery.brewery_type}</div>
@@ -68,7 +78,7 @@ function renderBrewery(brewery) {
 }
 
 function renderBreweries() {
-  state.breweries.forEach((brewery) => renderBrewery(brewery));
+  state.breweries.forEach((brewery) => renderBrewery(brewery)); // if not micro, regional or brewpub, skip rendering
 }
 
 render = () => {
@@ -78,10 +88,6 @@ render = () => {
 };
 
 initalise();
-
-
-
-
 
 // fetch("https://api.openbrewerydb.org/breweries?by_state=new_york&per")
 // .then(resp => resp.json())
